6ce9ab03404d1515443fc072717527c4b486c769
regibayoan <regi.bayoan@gmail.com>
2020-07-13 22:35:13 +0100

    Maze Solver project

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,15 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 /* /Users/RegiBayoan/eclipse-workspace/MySweetProgram
 */
 public class MySweetProgram {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, Please enter your name:");
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		
-		float d = 5.5f;
-		System.out.println(d);
+		Scanner in = new Scanner(new File("/Users/RegiBayoan/eclipse-workspace/MySweetProgram/src/students.txt"));
+		
+		List<String> students = new ArrayList<String>();
+		
+		while(in.hasNextLine()) {
+			students.add(in.nextLine());
 	}
+		for(int i = 0; i < students.size(); i++) {
+			System.out.println("Student " + i + ": " + students.get(i));
+		}
+		in.close();
 
+}
 }
