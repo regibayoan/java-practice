bed095d8e8be97f3729395802199bfd097d9d6c3
regibayoan <regi.bayoan@gmail.com>
2020-07-13 13:21:15 +0100

    Initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+/* /Users/RegiBayoan/eclipse-workspace/MySweetProgram
+*/
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Please enter your name:");
+		
+		
+		float d = 5.5f;
+		System.out.println(d);
+	}
+
+}
